98559896829624caeafdbe1707c337f6cc625ab5
TangTuTy <tangtang8366@gmail.com>
2022-08-10 22:12:38 +0700

    งานexercise4

---
+++ Exercise4_Kongpop_K.py
@@ -0,0 +1,10 @@
+a = 75
+b = 70
+c = 83
+d = 65
+
+print("คะแนนเก็บในแต่ละรายวิชาของคุณ")
+print("Foundation English =",a,"point")
+print("General Business =",b,"point")
+print("Introduction to Computer Systems =",c,"point")
+print("Computer Programming =",d,"point")
