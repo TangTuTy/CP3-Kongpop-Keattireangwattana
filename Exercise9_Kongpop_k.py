83882c0d04014396a31ba2565ea777fe40ba3a60
TangTuTy <tangtang8366@gmail.com>
2022-08-13 23:07:55 +0700

    Exercise9

---
+++ Exercise9_Kongpop_k.py
@@ -0,0 +1,7 @@
+UsernameInput = input("Username : ")
+PasswordInput = input("Password : ")
+while UsernameInput != "admin" or PasswordInput !="1234":
+    print("Please Try Again")
+    UsernameInput = input("Username : ")
+    PasswordInput = input("Password : ")
+print("DONe!")
