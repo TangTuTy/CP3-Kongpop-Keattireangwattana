84a0a1f75d16e3491cebc7293504afd0919bf4c7
TangTuTy <tangtang8366@gmail.com>
2022-08-14 21:38:00 +0700

    Lectur50

---
+++ Lectur50_Kongpop_K.py
@@ -0,0 +1,20 @@
+def บวก(x,y):
+    print("x + y =",x+y)
+
+def ลบ(x,y):
+    print("x - y =",x-y)
+
+def คูณ(x,y):
+    print("x * y =",x*y)
+
+def หาร(x,y):
+    print("x / y =",x/y)
+
+print("หาผลบวก ลบ คูณ หาร ต่างๆ")
+x = int(input("เลขตัวแรก   :"))
+y = int(input("เลขตัวที่สอง :"))
+
+บวก(x,y)
+ลบ(x,y)
+คูณ(x,y)
+หาร(x,y)
